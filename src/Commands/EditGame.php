b17fbd3fab7bb38dedc72dde1907d657fdf25f1f
Andrew Masiye <amasiye313@gmail.com>
2026-02-22 09:31:03 +0200

    feat(editor): refactor EditGame and Editor classes for improved configuration handling and initialization

--- src/Commands/EditGame.php
+++ src/Commands/EditGame.php
@@ -3,6 +3,8 @@
 namespace Sendama\Console\Commands;
 
 use Sendama\Console\Editor\Editor;
+use Sendama\Console\Exceptions\IOException;
+use Sendama\Console\Util\Config\ProjectConfig;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -10,27 +12,37 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
-    name: 'edit:game',
+    name: 'edit',
     description: 'Edit the game configuration.',
-    aliases: ['edit']
+    aliases: ['e', 'edit:game']
 )]
 class EditGame extends Command
 {
     public function configure(): void
     {
-        $this->addOption('directory', 'd', InputArgument::OPTIONAL, 'The directory of the game');
+        $this
+            ->addOption('directory', 'd', InputArgument::OPTIONAL, 'The directory of the game');
     }
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     * @throws IOException
+     */
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->writeln("Opening game configuration for editing...");
+        $output->writeln("Opening game configuration for editing...", OutputInterface::VERBOSITY_VERBOSE);
 
-        $directory = $input->getOption('directory');
+        $directory = $input->getOption('directory') ?? '.';
 
-        $editor = new Editor();
+        $projectConfig = new ProjectConfig($input, $output);
+        $projectConfig->load();
 
+        $editor = new Editor(name: $projectConfig->get("name"), workingDirectory: $directory);
+        $editor->run();
 
-
+        $output->writeln("Finished editing game configuration.", OutputInterface::VERBOSITY_VERBOSE);
         return Command::SUCCESS;
     }
 }
--- src/Editor/Editor.php
+++ src/Editor/Editor.php
@@ -2,23 +2,38 @@
 
 namespace Sendama\Console\Editor;
 
-use Atatusoft\Termutil\Events\Event;
+use Assegai\Collections\ItemList;
 use Atatusoft\Termutil\Events\Interfaces\ObservableInterface;
-use Atatusoft\Termutil\Events\Interfaces\StaticObserverInterface;
 use Atatusoft\Termutil\Events\Traits\ObservableTrait;
 use Atatusoft\Termutil\IO\Console\Console;
 use Atatusoft\Termutil\UI\Windows\Window;
 use Sendama\Console\Debug\Debug;
+use Sendama\Console\Editor\Enumerations\ChronoUnit;
+use Sendama\Console\Editor\Events\EditorEvent;
+use Sendama\Console\Editor\Events\Enumerations\EventType;
 use Sendama\Console\Editor\Interfaces\EditorStateInterface;
+use Sendama\Console\Editor\IO\InputManager;
 use Sendama\Console\Editor\States\EditorState;
 use Sendama\Console\Editor\States\EditorStateContext;
 use Sendama\Console\Editor\States\EditState;
 use Sendama\Console\Editor\States\ModalState;
 use Sendama\Console\Editor\States\PlayState;
 use Sendama\Console\Editor\States\ProjectBrowserState;
+use Sendama\Console\Editor\Widgets\AssetsPanel;
+use Sendama\Console\Editor\Widgets\HierarchyPanel;
+use Sendama\Console\Editor\Widgets\InspectorPanel;
+use Sendama\Console\Editor\Widgets\Widget;
+use Sendama\Console\Exceptions\IOException;
+use Sendama\Console\Exceptions\SendamaConsoleException;
+use Symfony\Component\Console\Output\ConsoleOutput;
 use Throwable;
 
-class Editor implements ObservableInterface
+/**
+ * The Sendama Editor application.
+ *
+ * @package Sendama\Console\Editor
+ */
+final class Editor implements ObservableInterface
 {
     use ObservableTrait;
 
@@ -47,19 +62,59 @@ class Editor implements ObservableInterface
      * @var int The frame rate of the game.
      */
     protected int $frameRate = 0;
-
+    /**
+     * @var bool Specifies whether debug mode is enabled or not.
+     */
     protected bool $isDebugMode {
         get {
-            return $this->settings?->isDebugMode;
+            return $this->gameSettings?->isDebugMode;
         }
     }
-    protected ?GameSettings $settings = null;
+    /**
+     * @var bool Determines whether the debug info panel is visible or hidden.
+     */
     private bool $showDebugInfo = false;
+    /**
+     * @var Window
+     */
     private Window $debugWindow;
+    /**
+     * @var EditorSettings The editor settings
+     */
+    protected EditorSettings $settings;
+    /**
+     * @var GameSettings|null The game settings.
+     */
+    protected ?GameSettings $gameSettings = null;
+    /**
+     * @var ProjectBrowserState
+     */
     protected ProjectBrowserState $projectBrowserState;
+    /**
+     * @var EditState
+     */
     protected EditState $editState;
+    /**
+     * @var PlayState
+     */
     protected PlayState $playState;
+    /**
+     * @var ModalState
+     */
     protected ModalState $modalState;
+    /**
+     * @var SplashScreen
+     */
+    protected SplashScreen $splashScreen;
+
+    /** Panels */
+    /**
+     * @var ItemList<Widget>
+     */
+    protected ItemList $panels;
+    protected HierarchyPanel $hierarchyPanel;
+    protected AssetsPanel $assetsPanel;
+    protected InspectorPanel $inspectorPanel;
 
     /**
      * @param string $name
@@ -71,13 +126,33 @@ class Editor implements ObservableInterface
     )
     {
         try {
+            register_shutdown_function(function () {
+                $this->finish();
+            });
+
             $this->initializeObservers();
             $this->configureErrorAndExceptionHandlers();
+            $this->initializeSettings();
+            $this->initializeManagers();
             $this->initializeConsole();
+            $this->initializeWidgets();
             $this->initializeEditorStates();
+            $this->splashScreen = new SplashScreen(
+                Console::cursor(),
+                new ConsoleOutput(),
+                $this->gameSettings
+            );
         } catch (Throwable $exception) {
             $this->handleException($exception);
         }
+    }
+
+    /**
+     * Called when this editor construct is destroyed.
+     */
+    public function __destruct()
+    {
+        $this->finish();
     }
 
     /**
@@ -86,6 +161,7 @@ class Editor implements ObservableInterface
      *
      * @param string $directory The working directory to set for the editor.
      * @return $this
+     * @throws IOException
      */
     public function setWorkingDirectory(string $directory): self
     {
@@ -104,27 +180,86 @@ class Editor implements ObservableInterface
         return $this;
     }
 
+    /**
+     * Starts the editor.
+     *
+     * @return void
+     * @throws IOException
+     */
     public function start(): void
     {
         Debug::info("Starting editor");
 
         Console::saveSettings();
 
-        Console::setName($this->settings?->name ?? "Sendama Editor | Unknown Game");
+        Console::setName($this->gameSettings?->name ?? "Sendama Editor | Unknown Game");
+
+        Console::setSize($this->gameSettings?->width, $this->gameSettings?->height);
+
+        Console::cursor()->hide();
+
+        InputManager::disableEcho();
+
+        InputManager::enableNonBlockingMode();
+
+        $this->splashScreen->show();
+
+        $this->addObservers(Time::class);
 
         $this->isRunning = true;
+
+        $this->notify(new EditorEvent(EventType::EDITOR_STARTED->value, $this));
+
+        Debug::info("Editor started");
     }
 
+    /**
+     * @return void
+     * @throws IOException
+     */
     public function stop(): void
     {
+        Console::reset();
+
+        Debug::info("Stopping editor");
+
+        InputManager::disableNonBlockingMode();
+
+        InputManager::enableEcho();
+
+        Console::cursor()->show();
+
+        $this->removeObservers(...$this->observers, ...$this->staticObservers);
+
         $this->isRunning = false;
+
+        $this->notify(new EditorEvent(EventType::EDITOR_STOPPED->value, $this));
+
+        Debug::info("Editor stopped");
     }
 
+    /**
+     * @return void
+     */
     public function finish(): void
     {
-        // TODO: Implement finish() method.
+        Debug::info("Shutting down editor");
+
+        Console::restoreSettings();
+
+        if ($lastError = error_get_last()) {
+            $this->handleError($lastError["type"], $lastError["message"], $lastError["file"], $lastError["line"]);
+        }
+
+        $this->notify(new EditorEvent(EventType::EDITOR_FINISHED->value, $this));
+
+        Debug::info("Editor shutdown complete");
     }
 
+    /**
+     * @return void
+     * @throws IOException
+     */
     public function run(): void
     {
         $sleepTime =(int)(1000000 / self::FPS);
@@ -133,7 +268,7 @@ class Editor implements ObservableInterface
         $lastFrameCountSnapShot = $this->frameCount;
 
         Debug::info("Running editor");
-        while (!$this->isRunning) {
+        while ($this->isRunning) {
             $this->handleInput();
             $this->update();
 
@@ -151,42 +286,77 @@ class Editor implements ObservableInterface
                 $nextFrameTime = microtime(true) + 1;
             }
         }
-
-        $this->finish();
     }
 
     public function setState(EditorStateInterface $editorState): void
     {
-        $context = new EditorStateContext($this->settings);
+        $context = new EditorStateContext(
+            $this->settings,
+            $this->gameSettings,
+            [
+                'hierarchy' => $this->hierarchyPanel,
+                'assets' => $this->assetsPanel
+            ]
+        );
 
         $this->editorState?->exit($context);
         $this->editorState = $editorState;
         $this->editorState->enter($context);
     }
 
+    /**
+     * Handle editor input
+     *
+     * @return void
+     */
     private function handleInput(): void
     {
+        InputManager::handleInput();
+
+        $this->notify(new EditorEvent(EventType::EDITOR_INPUT_HANDLED->value, $this));
     }
 
+    /**
+     * Update the editor state.
+     *
+     * @return void
+     */
     private function update(): void
     {
         $this->editorState->update();
+
+        foreach ($this->panels as $panel) {
+            $panel->update();
+        }
+
+        $this->notify(new EditorEvent(EventType::EDITOR_UPDATED->value, $this));
     }
 
     private function render(): void
     {
         $this->frameCount++;
         $this->editorState->render();
+        foreach ($this->panels as $panel) {
+            $panel->render();
+        }
         $this->renderDebugInfo();
+
+        $this->notify(new EditorEvent(EventType::EDITOR_RENDERED->value, $this));
     }
 
     private function renderDebugInfo(): void
     {
         if ($this->isDebugMode && $this->showDebugInfo) {
+            $content = ["FPS: $this->frameRate, Delta: " . round(Time::getDeltaTime() / 2), "Time: " . Time::getPrettyTime(ChronoUnit::SECONDS)];
 
+            $this->debugWindow->content = $content;
+            $this->debugWindow->render();
         }
     }
 
+    /**
+     * @return void
+     */
     protected function configureErrorAndExceptionHandlers(): void {
         error_reporting(E_ALL);
 
@@ -210,14 +380,14 @@ class Editor implements ObservableInterface
     private function handleException(Throwable $exception): never
     {
         Debug::error($exception);
-        $this->stop();
 
-        if ($this->settings->isDebugMode) {
+        if ($this->gameSettings?->isDebugMode) {
             exit($exception);
         }
 
         exit("$exception\n");
     }
+
     /**
      * Handles game errors.
      *
@@ -231,9 +401,8 @@ class Editor implements ObservableInterface
     {
         $errorMessage = "[$errno] $errstr in $errfile on line $errline";
         Debug::error($errorMessage);
-        $this->stop();
 
-        if ($this->settings->isDebugMode) {
+        if ($this->gameSettings?->isDebugMode) {
             exit($errorMessage);
         }
 
@@ -243,8 +412,8 @@ class Editor implements ObservableInterface
     protected function initializeConsole(): void
     {
         Console::init([
-            "width" => $this->settings->width ?? DEFAULT_TERMINAL_WIDTH,
-            "height" => $this->settings->height ?? DEFAULT_TERMINAL_HEIGHT,
+            "width" => $this->gameSettings?->width ?? DEFAULT_TERMINAL_WIDTH,
+            "height" => $this->gameSettings?->height ?? DEFAULT_TERMINAL_HEIGHT,
         ]);
     }
 
@@ -255,6 +424,42 @@ class Editor implements ObservableInterface
         $this->playState = new PlayState($this);
         $this->modalState = new ModalState($this);
 
-        $this->setState($this->projectBrowserState);
+        $this->setState($this->editState);
+    }
+
+    /**
+     * @return void
+     * @throws SendamaConsoleException
+     */
+    private function initializeSettings(): void
+    {
+        $this->settings = EditorSettings::loadFromDirectory($this->workingDirectory);
+        $this->gameSettings = GameSettings::loadFromDirectory($this->workingDirectory);
+    }
+
+    /**
+     * @return void
+     */
+    private function initializeManagers(): void
+    {
+        InputManager::init();
+    }
+
+    /**
+     * @return void
+     */
+    private function initializeWidgets(): void
+    {
+        $this->panels = new ItemList(Widget::class);
+        $halfHeight = (int)(($this->settings->height - 1) / 2);
+        $this->hierarchyPanel = new HierarchyPanel(height: $halfHeight);
+        $this->assetsPanel = new AssetsPanel(position: ['x' => 1, 'y' => $halfHeight + 1], height: $halfHeight);
+        $centralPanelWidth = $this->settings->width - 2 - (35 * 2);
+
+        $this->inspectorPanel = new InspectorPanel(position: ['x' => ($centralPanelWidth + 35), 'y' => 1], height: $this->settings->height - 1);
+
+        $this->panels->add($this->hierarchyPanel);
+        $this->panels->add($this->assetsPanel);
+        $this->panels->add($this->inspectorPanel);
     }
 }
